04f7417dce1ee85c7612258749ca71ec25a8aa32
mehulbhange <bhangemehul@gmail.com>
2022-09-26 22:14:17 +0530

    ADDED:[Mehul] find contact and edit the contact

--- AddressBook.js
+++ AddressBook.js
@@ -75,15 +75,89 @@ let addContact = (contact) => {
     console.log("Contact Added Successfully!!")
 }
 
+let editContact = () => {
+    let frstName = prompt("Enter First Name : ");
+    let lstName = prompt("Enter Lastt Name : ");
+    let index = addressBookArray.findIndex(contact => contact.firstName == frstName && contact.lastName == lstName);
+    if (index == -1)
+        console.log("Could not find the contact!!")
+    else {
+        //addressBookArray[index] = getContact();
+        console.log("\n1.First Name \n2.Last name \n3.Address \n4.City \n5.State \n6.Zip \n7.Phone Number \n8.Email \n");
+        let ch = Number(prompt("What you want to edit ?"));
+        switch(ch){
+            case 0:
+                console.log("Done!!!");
+                break;
+            case 1:
+                let firstName = prompt("Enter First Name ");
+                if(!nameRegex.test(firstName))
+                    throw "Invalid first name";
+                addressBookArray[index].firstName = firstName;
+                break;
+            case 2:
+                let lastName = prompt("Enter Last Name ");
+                if(!nameRegex.test(lastName))
+                    throw "Invalid last name";
+                addressBookArray[index].lastName = lastName;
+                break;
+            case 3:
+                let address = prompt("Enter Address ");
+                if(!addressRegex.test(address))
+                    throw "Invalid Address!!";
+                addressBookArray[index].address = address;
+                break;
+            case 4:
+                let city = prompt("Enter City ");
+                if(!cityStateRegex.test(city))
+                    throw "Invalid City!!";
+                addressBookArray[index].city = city;                    
+                break;
+            case 5:
+                let state = prompt("Enter State ");
+                if(!cityStateRegex.test(state))
+                    throw "Invalid State!!"
+                addressBookArray[index].state = state;     
+                break;
+            case 6:
+                let zip = prompt("Enter Zip ");
+                if(!zipRegex.test(zip))
+                    throw "Invalid Zip!!";
+                addressBookArray[index].zip = zip;     
+                break;
+            case 7:
+                let phoneNumber = prompt("Enter Phone Number ");
+                if(!phoneNumberRegex.test(phoneNumber))
+                    throw "Invalid Phone number!!"
+                addressBookArray[index].phoneNumber = phoneNumber;     
+                break;
+            case 8:
+                let email = prompt("Enter Email ");
+                if(!emailRegex.test(email))
+                    throw "Invalid Email!!";
+                addressBookArray[index].email = email;     
+                break;
+            default:
+                "Invalid Input"           
+        }
+        console.log("Contact edited successfully!!");
+    }
+}
+
 do {
-    console.log("\n0.Exit \n1.Add Contact");
+    console.log("\n0.Exit \n1.Add Contact \n2.Edit Contact");
     choice = Number(prompt("Enter Your Choice "));
     switch (choice) {
-        case 0: 
-            return;
         case 1: 
             try{
                 addContact(getContact());
+            }catch(ex){
+                console.log(ex);
+            }
+            break;
+        case 2:
+            try{
+                editContact();
             }catch(ex){
                 console.log(ex);
             }
@@ -92,4 +166,4 @@ do {
             console.log("Invalid Choice !!");
     }
 
-} while (choice != 2)
+} while (choice != 0)
